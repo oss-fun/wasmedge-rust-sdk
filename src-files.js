9711e54af28622e29b7996c293ec9bfaeeaf48b8
L-jasmine <L-jasmine@users.noreply.github.com>
2024-07-30 08:46:01 +0000

    deploy: 3b1e9299d3bd965ac94751cbb16ca65190ec79f6

---
+++ src-files.js
@@ -0,0 +1,4 @@
+var srcIndex = JSON.parse('{\
+"async_wasi":["",[["snapshots",[["common",[["net",[],["async_tokio.rs","mod.rs"]],["vfs",[],["impls.rs","mod.rs","virtual_sys.rs"]]],["clock.rs","error.rs","memory.rs","mod.rs","types.rs"]],["preview_1",[],["async_poll.rs","async_socket.rs","mod.rs"]]],["env.rs","mod.rs"]]],["lib.rs"]]\
+}');
+createSrcSidebar();
